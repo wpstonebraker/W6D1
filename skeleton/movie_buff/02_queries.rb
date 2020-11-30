383814572c2d6abee13cce06f586d81a1d2c63e5
Mike Criollo <mkcriollo@gmail.com>
2020-11-30 16:04:09 -0500

    finish quieres 02

--- skeleton/movie_buff/02_queries.rb
+++ skeleton/movie_buff/02_queries.rb
@@ -2,18 +2,31 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+Movie
+  .select(:id,:title,:yr,:score)
+  .where(yr: 1980..1989)
+  .group(:id)
+  .having(score: 3..5)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
+  Movie
+    .select(:yr)
+    .group(:yr)
+    .having('MAX(score) < 8')
+    .pluck(:yr)
 
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
+  Actor
+    .joins(:movies,:castings)
+    .where(movies: {title: title})
+    .order('castings.ord ASC')
+    .group('castings.ord')
 end
 
 def vanity_projects
